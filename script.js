f82ba4c6330f49ef73711140d5284010f27ca07a
GauravFrontend <gaurav9911093928@gmail.com>
2023-11-30 02:35:57 +0530

    Added Mouse Follower

--- script.js
+++ script.js
@@ -25,3 +25,56 @@
     });
   });
   
+// *********************** cursor ***********************
+let innerCursor = document.querySelector(".inner-cursor");
+let outercursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outercursor.style.left = `${x}px`;
+    outercursor.style.top = `${y}px`;
+
+
+}
+let namee = document.querySelector("#name");
+namee.addEventListener("mouseover",() => {
+    innerCursor.classList.add("grow");
+    outercursor.classList.add("hidouter");
+});
+namee.addEventListener("mouseleave",() => {
+    innerCursor.classList.remove("grow");
+    outercursor.classList.remove("hidouter");
+});
+let aboutleft = document.querySelector("#aboutleft");
+aboutleft.addEventListener("mouseover",() => {
+    innerCursor.classList.add("grow");
+    outercursor.classList.add("hidouter");
+});
+aboutleft.addEventListener("mouseleave",() => {
+    innerCursor.classList.remove("grow");
+    outercursor.classList.remove("hidouter");
+});
+let work = document.querySelector("#work");
+work.addEventListener("mouseover",() => {
+    innerCursor.classList.add("grow");
+    outercursor.classList.add("hidouter");
+});
+work.addEventListener("mouseleave",() => {
+    innerCursor.classList.remove("grow");
+    outercursor.classList.remove("hidouter");
+});
+let getintouch = document.querySelector("#getintouch");
+getintouch.addEventListener("mouseover",() => {
+    innerCursor.classList.add("grow");
+    outercursor.classList.add("hidouter");
+});
+getintouch.addEventListener("mouseleave",() => {
+    innerCursor.classList.remove("grow");
+    outercursor.classList.remove("hidouter");
+});
+// *********************** cursor ***********************
